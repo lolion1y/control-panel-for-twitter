5937dd1dca8480dc52391d8e1b521784c95adf0c
lolion1y <93830081+lolion1y@users.noreply.github.com>
2025-10-31 17:25:13 +0800

    bump version: 200.3

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     200.2
+// @version     200.3
 // ==/UserScript==
 void function() {
 
