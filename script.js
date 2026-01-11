b09c6d4ddc7760617a9de32111d6e439e4d28086
lolion1y <93830081+lolion1y@users.noreply.github.com>
2026-01-12 02:48:36 +0800

    sync: version `4.19.4`
    sync: version `4.19.3`

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     4.19.2
+// @version     4.19.4
 // ==/UserScript==
 void function() {
 
