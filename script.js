3b6399e6d5396f81b0c3b9ab522ed4ecff98f0d5
lolion1y <93830081+lolion1y@users.noreply.github.com>
2026-02-06 12:31:44 +0800

    bump version: 4.21.1.2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     4.21.1.1
+// @version     4.21.1.2
 // ==/UserScript==
 void function() {
 
