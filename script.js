8d6c8420bc96c82f8cf958421ef529c1810a1496
lolion1y <93830081+lolion1y@users.noreply.github.com>
2026-07-05 14:01:03 +0800

    sync version: 4.23.0

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     4.22.5
+// @version     4.23.0
 // ==/UserScript==
 void function() {
 
